da7d23302001fcb60bb9248ff7905612d1d2dac1
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 20:23:47 +0100

    Getting endpoints from Github

--- src/lang/lang.service.ts
+++ src/lang/lang.service.ts
@@ -6,4 +6,17 @@ export class LangService {
 
     constructor(private readonly prismaService: PrismaService) {}
 
+    async getLanguageEndpoint(): Promise<{endpoints: []}> {
+
+        const response = await fetch("https://raw.githubusercontent.com/DFG-Dev/localizationFiles/main/endpoints.json", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        return response.json()
+
+    }
+
 }
