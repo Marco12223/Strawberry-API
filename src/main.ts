52f597fdf6ae166ac320d78f92c7db50f9f97bc8
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 18:40:58 +0100

    Add BearerAuth to SwaggerUI

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,22 @@ async function bootstrap() {
       .setDescription(pack.description)
       .setVersion(pack.version)
       .addServer('http://localhost:3000', 'Local environment')
+      .addBearerAuth({
+        type: 'oauth2',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        in: 'header',
+        name: 'Authorization token',
+        description: 'Enter your JWT token in the format "Bearer {token}"'
+      }, 'access_token')
+      .addSecurityRequirements("access_token")
       .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup('swagger', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(3000);
 
