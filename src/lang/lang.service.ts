06afbbabd222200c9b8d2b2c1526f6531edc1a8c
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 20:28:33 +0100

    Creating `getLanguageData` function

--- src/lang/lang.service.ts
+++ src/lang/lang.service.ts
@@ -6,9 +6,24 @@ export class LangService {
 
     constructor(private readonly prismaService: PrismaService) {}
 
-    private async getLanguageEndpoint(): Promise<{endpoints: []}> {
+    private async getLanguageEndpoint(): Promise<{endpoints: [{name: string, locale: string, url: string}]}> {
 
         const response = await fetch("https://raw.githubusercontent.com/DFG-Dev/localizationFiles/main/endpoints.json", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        return response.json()
+
+    }
+
+    async getLanguageData(locale: string): Promise<{data: []}> {
+
+        const endpoints = await this.getLanguageEndpoint();
+        const endpoint = endpoints.endpoints.find(e => e.locale === locale);
+        const response = await fetch(endpoint.url, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json"
