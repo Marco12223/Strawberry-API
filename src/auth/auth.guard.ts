a704a343078b96d4f77267c5ffbb4a3e0719fc07
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 18:17:01 +0100

    Creating logic to authorized a request

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,11 +1,41 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {CanActivate, ExecutionContext, Injectable, UnauthorizedException} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import {JwtService} from "@nestjs/jwt";
+import { Request } from 'express';
+import {jwtConstants} from "./constants";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+
+  constructor(private jwtService: JwtService) {}
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+
+    try {
+      const payload = this.jwtService.verifyAsync(
+          token,
+          {
+            secret: jwtConstants.secret
+          });
+
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException('Unauthorized');
+    }
     return true;
+
   }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+
 }
