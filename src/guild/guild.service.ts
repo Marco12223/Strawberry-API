f0e9c4d35853bb88fe6baba9cc8c5f6b5faadc63
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 19:19:42 +0100

    Creating Get/Update/Delete functions

--- src/guild/guild.service.ts
+++ src/guild/guild.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import {PrismaService} from "../primsa/prisma.service";
 
 @Injectable()
-export class GuildService {}
+export class GuildService {
+
+    constructor(private readonly prismaService: PrismaService) {}
+
+    async getGuilds() {
+        return this.prismaService.guild.findMany();
+    }
+
+    async getGuild(id: number) {
+        return this.prismaService.guild.findUnique({
+            where: {
+                id: id
+            }
+        });
+    }
+
+    async createGuild(guild: any) {
+        return this.prismaService.guild.create({
+            data: guild
+        });
+    }
+
+    async updateGuild(id: number, guild: any) {
+        return this.prismaService.guild.update({
+            where: {
+                id: id
+            },
+            data: guild
+        });
+    }
+
+    async deleteGuild(id: number) {
+        return this.prismaService.guild.delete({
+            where: {
+                id: id
+            }
+        });
+    }
+
+}
