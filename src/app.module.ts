a3c32b4003db81ccbf0a8ce27f85be131e0c596c
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 18:07:25 +0100

    Add JwTModule and other Controller & Services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './primsa/prisma.service';
+import { AuthService } from './auth/auth.service';
+import { UsersService } from './users/users.service';
+import { AuthController } from './auth/auth.controller';
+import {JwtModule} from "@nestjs/jwt";
+import {jwtConstants} from "./auth/constants";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [
+      JwtModule.register({
+        global: true,
+        secret: jwtConstants.secret,
+        signOptions: {expiresIn: '2h'}
+      }),
+  ],
+  controllers: [AppController, AuthController],
+  providers: [AppService, PrismaService, AuthService, UsersService],
 })
 export class AppModule {}
