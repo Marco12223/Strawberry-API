2c3cd667ecbfcce8a12b74f207b2d98b35c428c2
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 20:45:09 +0100

    Creating `available-languages` & `:locale` endpoints

--- src/lang/lang.controller.ts
+++ src/lang/lang.controller.ts
@@ -1,9 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Param, UseGuards} from '@nestjs/common';
 import {LangService} from "./lang.service";
+import {ApiOperation, ApiParam, ApiResponse, ApiTags} from "@nestjs/swagger";
+import {AuthGuard} from "../auth/auth.guard";
 
+@ApiTags('Language Management')
 @Controller('lang')
 export class LangController {
 
     constructor(private readonly langService: LangService) {}
+
+    @Get('available-languages')
+    @UseGuards(AuthGuard)
+    @ApiOperation({summary: 'Get available languages', description: 'Get available languages, IMPORTANT: The user must be logged in to access this endpoint'})
+    @ApiResponse({status: 200, description: 'Languages found', schema: {example: {locales: ['string']}}})
+    @ApiResponse({status: 401, description: 'Unauthorized', schema: {example: {statusCode: 401, error: 'Unauthorized', message: "Unauthorized"}}})
+    @ApiResponse({status: 500, description: 'Internal Server Error', schema: {example: {statusCode: 500, error: 'Internal Server Error'}}})
+    async getAvailableLanguages(): Promise<{ locales: string[] }> {
+        return this.langService.getAvailableLanguages();
+    }
+
+    @Get(':locale')
+    @UseGuards(AuthGuard)
+    @ApiOperation({summary: 'Get language data', description: 'Get language data, IMPORTANT: The user must be logged in to access this endpoint'})
+    @ApiResponse({status: 200, description: 'Language data found', schema: {example: {}}})
+    @ApiResponse({status: 401, description: 'Unauthorized', schema: {example: {statusCode: 401, error: 'Unauthorized', message: "Unauthorized"}}})
+    @ApiResponse({status: 500, description: 'Internal Server Error', schema: {example: {statusCode: 500, error: 'Internal Server Error'}}})
+    @ApiParam({name: 'locale', description: 'Locale', required: true, schema: {type: 'string'}})
+    async getLanguageData(@Param('locale') locale: string): Promise<{}> {
+        return this.langService.getLanguageData(locale);
+    }
 
 }
