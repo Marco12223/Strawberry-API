5740e608db47dc1607064893320d7730ae1c0cb9
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 19:09:06 +0100

    Change status code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,8 +10,8 @@ export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('login')
-    @ApiOperation({summary: 'Login to the application'})
-    @ApiResponse({status: 200, description: 'User logged in successfully', schema: {example: {access_token: 'string'}}})
+    @ApiOperation({summary: 'Login to the application', description: 'Login to the application, IMPORTANT: The user must be registered by an admin before logging in is possible to the application'})
+    @ApiResponse({status: 201, description: 'User logged in successfully', schema: {example: {access_token: 'string'}}})
     @ApiBody({schema: {example: {username: 'string', password: 'string'}}})
     login(@Body() body: {username: string, password: string}){
         return this.authService.signIn(body.username, body.password);
