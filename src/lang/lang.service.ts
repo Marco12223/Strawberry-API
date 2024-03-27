258ec5b71835b0c82a396cd668a5f888df914b3f
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 20:25:04 +0100

    Make `getLanguageEndpoint` private

--- src/lang/lang.service.ts
+++ src/lang/lang.service.ts
@@ -6,7 +6,7 @@ export class LangService {
 
     constructor(private readonly prismaService: PrismaService) {}
 
-    async getLanguageEndpoint(): Promise<{endpoints: []}> {
+    private async getLanguageEndpoint(): Promise<{endpoints: []}> {
 
         const response = await fetch("https://raw.githubusercontent.com/DFG-Dev/localizationFiles/main/endpoints.json", {
             method: "GET",
