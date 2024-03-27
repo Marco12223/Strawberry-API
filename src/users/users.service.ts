4ff7a48ea07e8a35b24b665572f5e580d20c7e1f
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 17:22:01 +0100

    Fix something

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import {PrismaService} from "../primsa/prisma.service";
-import {Prisma} from "@prisma/client";
+import {apiUsers, Prisma} from "@prisma/client";
 import {Users} from "./users.entity";
 
 @Injectable()
 export class UsersService {
 
-    constructor(private readonly prismaService: PrismaService) {
-    }
+    constructor(private readonly prismaService: PrismaService) {}
 
-    async findOne(username: Prisma.apiUsersWhereUniqueInput): Promise<any> {
-        return this.prismaService.apiUsers.findUnique({
-            where: username
-        });
+    async findOne(username: Prisma.apiUsersFindUniqueArgs): Promise<Users|undefined> {
+        return this.prismaService.apiUsers.findUnique(username);
     }
 
 }
