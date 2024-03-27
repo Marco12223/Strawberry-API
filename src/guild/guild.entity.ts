5fa3f9849e149c983593eea609fd1757a298e0cd
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 19:17:54 +0100

    Add entries for entity

---
+++ src/guild/guild.entity.ts
@@ -0,0 +1,7 @@
+import { Prisma } from '@prisma/client';
+
+export class Guild implements Prisma.guildCreateInput {
+    features: Prisma.guildCreatefeaturesInput | string[];
+    guildId: string;
+    language: string;
+}
