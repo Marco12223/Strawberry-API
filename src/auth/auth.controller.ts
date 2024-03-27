2ceea48dfb66c9907fd7614b61a273b48b71c441
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 19:39:10 +0100

    add 500 (`Internal server error`) to OpenAPI

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,9 +9,10 @@ export class AuthController {
 
     constructor(private authService: AuthService) {}
 
-    @Post('login')
+    @Post('')
     @ApiOperation({summary: 'Login to the application', description: 'Login to the application, IMPORTANT: The user must be registered by an admin before logging in is possible to the application'})
     @ApiResponse({status: 201, description: 'User logged in successfully', schema: {example: {access_token: 'string'}}})
+    @ApiResponse({status: 500, description: 'Internal server error', schema: {example: {statusCode: 500, message: 'Internal server error'}}})
     @ApiBody({schema: {example: {username: 'string', password: 'string'}}})
     login(@Body() body: {username: string, password: string}){
         return this.authService.signIn(body.username, body.password);
