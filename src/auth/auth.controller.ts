8196c74a564cb165d11d51c691d3459696b3d1ee
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 19:02:24 +0100

    Add OpenAPI things

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,18 @@
 import {Body, Controller, Post} from '@nestjs/common';
 import {AuthService} from "./auth.service";
+import {ApiBody, ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 
+
+@ApiTags('Authorization')
 @Controller('auth')
 export class AuthController {
 
     constructor(private authService: AuthService) {}
 
     @Post('login')
+    @ApiOperation({summary: 'Login to the application'})
+    @ApiResponse({status: 200, description: 'User logged in successfully', schema: {example: {access_token: 'string'}}})
+    @ApiBody({schema: {example: {username: 'string', password: 'string'}}})
     login(@Body() body: {username: string, password: string}){
         return this.authService.signIn(body.username, body.password);
     }
