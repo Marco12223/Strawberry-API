417c0347bd03e672438d979f8e71cfa2432225bb
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 18:09:36 +0100

    Fixing some problems

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,8 +8,8 @@ export class UsersService {
 
     constructor(private readonly prismaService: PrismaService) {}
 
-    async findOne(username: Prisma.apiUsersWhereUniqueInput): Promise<Users|undefined> {
-        return this.prismaService.apiUsers.findUnique({where: username});
+    async findOne(where: Prisma.apiUsersWhereUniqueInput): Promise<Users|undefined> {
+        return this.prismaService.apiUsers.findUnique({where});
     }
 
 }
