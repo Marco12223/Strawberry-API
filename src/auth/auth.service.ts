0c4e7ed6ce77d3b412b0aaa610fee3d0e98cec31
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-03-27 18:08:11 +0100

    Creating AuthService & method to create tokens

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import {Injectable, UnauthorizedException} from '@nestjs/common';
+import {UsersService} from "../users/users.service";
+import {Prisma} from "@prisma/client";
+import {JwtService} from "@nestjs/jwt";
+
+@Injectable()
+export class AuthService {
+    constructor(private usersService: UsersService, private  jwtService: JwtService) {}
+
+    async signIn(username: string, password: string): Promise<any> {
+        const user = await this.usersService.findOne({username: username});
+
+        if(user && user.password !== password) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const payload = { username: user.username, sub: user.id };
+
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+
+    }
+
+}
