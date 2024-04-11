4f811224b371f73179f20c1a01746c63ca90ce28
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-04-11 21:48:28 +0200

    Updating entity

--- src/guild/guild.entity.ts
+++ src/guild/guild.entity.ts
@@ -1,7 +1,6 @@
 import {Prisma} from '@prisma/client';
 
 export class Guild implements Prisma.guildCreateInput {
-    features: Prisma.guildCreatefeaturesInput | string[];
     guildId: string;
     language: string;
 }
