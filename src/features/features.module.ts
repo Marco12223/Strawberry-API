b87c1da67da0d89994c3b705a88abbf734342668
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-04-12 14:40:17 +0200

    Creating endpoints & add functions to service

--- src/features/features.module.ts
+++ src/features/features.module.ts
@@ -3,10 +3,11 @@ import { LogsService } from './logs/logs.service';
 import { LogsController } from './logs/logs.controller';
 import {PrismaService} from "../prisma/prisma.service";
 import { AutoroleService } from './autorole/autorole.service';
+import { AutoroleController } from './autorole/autorole.controller';
 
 @Module({
     imports: [],
-    controllers: [LogsController],
+    controllers: [LogsController, AutoroleController],
     providers: [LogsService, PrismaService, AutoroleService],
 })
 export class FeaturesModule {}
