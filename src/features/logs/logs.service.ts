0ef3cf119a4f02756e2f994b1e5fdf0adebb5184
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-04-12 16:24:40 +0200

    Recoding complete service file

--- src/features/logs/logs.service.ts
+++ src/features/logs/logs.service.ts
@@ -7,36 +7,56 @@ export class LogsService {
 
     constructor(private readonly prismaService: PrismaService) {}
 
-    async create(data: Prisma.featureLogsCreateInput) {
-        return this.prismaService.featureLogs.create({ data });
+    async get(where: Prisma.featureLogsWhereUniqueInput){
+        return this.prismaService.featureLogs.findUnique({where: where});
     }
 
-    async get(where: Prisma.featureLogsWhereUniqueInput) {
-        let logs = await this.prismaService.featureLogs.findUnique({ where });
-        if(logs === null) {
-            throw new HttpException({statusCode: HttpStatus.NOT_FOUND, error: 'Logs not found'}, HttpStatus.NOT_FOUND);
+    async exists(where: Prisma.featureLogsWhereUniqueInput){
+        return this.prismaService.featureLogs.findUnique({where: where}).then((data) => {
+            return {exists: data !== null};
+        });
+    }
+
+    async addType(where: Prisma.featureLogsWhereUniqueInput, type: string){
+        if(this.hasType(where, type)){
+            throw new HttpException('Type already exists', HttpStatus.CONFLICT);
         } else {
-            return logs
+            return this.prismaService.featureLogs.update({where: where, data: {type: {push: type}}});
         }
     }
 
-    async exists(where: Prisma.featureLogsWhereUniqueInput) {
-        let logs = await this.prismaService.featureLogs.findUnique({ where });
-        if(logs === null) {
-            return false;
+    async removeType(where: Prisma.featureLogsWhereUniqueInput, type: string) {
+        if(!this.hasType(where, type)){
+            throw new HttpException('Type not found', HttpStatus.NOT_FOUND);
         } else {
-            return true;
+            const featureLog = await this.prismaService.featureLogs.findUnique({
+                where: where,
+                select: {
+                    type: true
+                }
+            });
+
+            if (!featureLog) {
+                throw new Error('FeatureLog not found');
+            }
+
+            const updatedTypes = featureLog.type.filter(t => t !== type);
+
+            return this.prismaService.featureLogs.update({
+                where: where,
+                data: {
+                    type: {
+                        set: updatedTypes
+                    }
+                }
+            });
         }
     }
 
-    async delete(where: Prisma.featureLogsWhereUniqueInput) {
-        let logs = await this.prismaService.featureLogs.findUnique({ where });
-        if(logs === null) {
-            throw new HttpException({statusCode: HttpStatus.NOT_FOUND, error: 'Logs not found'}, HttpStatus.NOT_FOUND);
-        } else {
-            await this.prismaService.featureLogs.delete({ where })
-            return { message: 'Logs deleted', statusCode: 200 };
-        }
+    async hasType(where: Prisma.featureLogsWhereUniqueInput, type: string){
+        return this.prismaService.featureLogs.findUnique({where: where}).then((data) => {
+            return {hasType: data.type.includes(type)};
+        });
     }
 
 }
